4f1b99314ad1123421be754ba3fc2b0fe4f624ab
Parker Qi <parkerqi@Parkers-MacBook-Air-4.local>
2023-11-13 14:48:45 -0800

    changed ai interview js

--- src/pages/ai_conversation.js
+++ src/pages/ai_conversation.js
@@ -1,24 +1,109 @@
-import React from 'react';
-//import LandingPage from './pages/LandingPage'; // adjust the path according to your project structure
+import * as React from "react";
 
-function conversation_ai() {
-  return (
-    <div style={{ display: 'flex', height: '100vh' }}>
-      <div style={{ flex: 1, border: '1px solid black' }}>
-        <h2>Selection Area</h2>
-        {/* Add your selection items here */}
-      </div>
-      <div style={{ flex: 2, border: '1px solid black', display: 'flex', flexDirection: 'column' }}>
-        <div style={{ flex: 1, overflow: 'auto' }}>
-          <h2>Conversation Area</h2>
-          {/* Add your conversation items here */}
-        </div>
-        <div style={{ height: '50px', border: '1px solid black' }}>
-          <input type="text" placeholder="Type here..." style={{ width: '100%', height: '100%' }} />
-        </div>
-      </div>
-    </div>
-  );
-}
+function conversation_ai(props) {
+    const handleButtonClick = () => {
+        // handle button click event
+    };
 
+      return (
+        <div className="bg-white">
+          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+            <div className="flex flex-col items-stretch w-[18%] max-md:w-full max-md:ml-0">
+              <div className="flex-col overflow-hidden relative flex aspect-[0.2777777777777778] pt-60 pb-8 max-md:mt-10 max-md:pt-24">
+                <img
+                  loading="lazy"
+                  srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/a92ac8b4-b91e-4726-a0ce-be69775b42b7?apiKey=307f7ea7cf424aa8ac878e781b03700d&"
+                  className="absolute z-[-1] h-full w-full object-cover object-center inset-0"
+                />
+                <div className="relative items-stretch self-stretch flex flex-col">
+                  <div className="flex items-center justify-between gap-5">
+                    <div className="bg-teal-900 flex w-1 shrink-0 h-8 flex-col my-auto rounded-none" />
+                    <div className="justify-center items-stretch self-stretch flex grow basis-[0%] flex-col">
+                      <div className="text-zinc-900 text-sm font-semibold leading-5 whitespace-nowrap bg-lime-500 bg-opacity-10 items-center px-5 py-3 rounded-[90px]">
+                        Google APM
+                      </div>
+                    </div>
+                  </div>
+                  <div className="justify-center items-stretch flex w-[216px] max-w-full flex-col mt-2 self-end">
+                    <div className="text-zinc-900 text-sm leading-5 whitespace-nowrap bg-white items-center px-5 py-3 rounded-[90px]">
+                      Practice 1
+                    </div>
+                  </div>
+                </div>
+                <img
+                  loading="lazy"
+                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/f6ed7ca3-9e9e-4aa7-bc7c-5717304357c5?apiKey=307f7ea7cf424aa8ac878e781b03700d&"
+                  className="aspect-[250] object-contain object-center w-full fill-slate-200 overflow-hidden self-stretch mt-[473px] max-md:mt-10"
+                />
+                <div className="relative flex w-[125px] max-w-full items-stretch gap-2 ml-7 mt-6 px-5 max-md:ml-2.5">
+                  <div className="flex-col text-white text-xl font-black leading-7 relative whitespace-nowrap overflow-hidden aspect-square w-11 px-2.5 py-4 rounded-[50%]">
+                    <img
+                      loading="lazy"
+                      srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/c43529c6-742e-4dc9-82a7-a7bb245f5264?apiKey=307f7ea7cf424aa8ac878e781b03700d&"
+                      className="absolute z-[-1] h-full w-full object-cover object-center inset-0"
+                    />
+                    kr
+                  </div>
+                  <div className="text-zinc-900 text-base font-semibold leading-6 my-auto">
+                    Karina
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col items-stretch w-[82%] ml-5 max-md:w-full max-md:ml-0">
+              <div className="flex flex-col my-auto px-5 max-md:max-w-full max-md:mt-10">
+                <div className="items-stretch flex w-[289px] max-w-full justify-between gap-5 max-md:justify-center">
+                  <div className="text-zinc-900 text-xl font-semibold leading-7">
+                    Google APM
+                  </div>
+                  <div className="bg-gray-300 self-center flex w-px shrink-0 h-3.5 flex-col my-auto" />
+                  <div className="text-neutral-400 text-base leading-6 self-center whitespace-nowrap my-auto">
+                    edit
+                  </div>
+                </div>
+                <div className="text-white text-base leading-6 w-[716px] justify-center items-stretch border shadow-sm bg-green-800 max-w-full mt-24 px-5 py-4 rounded-xl border-solid border-teal-900 border-opacity-50 max-md:max-w-full max-md:mt-10 max-md:pr-2.5">
+                  <span className="">
+                    Based on the information you provided, I have generated the
+                    following questions for you. Let’s start!
+                    <br />
+                    <br />
+                  </span>
+                  <span className="font-semibold">
+                    Question: Imagine you are PM in Google, design a gardening
+                    product for Google.
+                  </span>
+                </div>
+                <div className="bg-zinc-300 self-stretch shrink-0 h-[3px] -mr-5 mt-96 max-md:max-w-full max-md:mt-10" />
+                <div className="self-stretch flex w-full justify-between gap-5 -mr-5 mt-10 max-md:max-w-full max-md:flex-wrap">
+                  <div className="flex items-stretch gap-5">
+                    <div className="items-center shadow-sm bg-green-800 flex justify-between gap-2 px-6 py-3 rounded-2xl max-md:px-5">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/c6ea10df-fb5b-46a9-8dff-dd310db937ad?apiKey=307f7ea7cf424aa8ac878e781b03700d&"
+                        className="aspect-square object-contain object-center w-5 overflow-hidden shrink-0 max-w-full my-auto"
+                      />
+                      <div className="text-white text-center text-xl font-semibold leading-7 self-stretch whitespace-nowrap">
+                        Answer
+                      </div>
+                    </div>
+                    <div className="items-center border shadow-sm bg-white self-center flex aspect-[1.619047619047619] flex-col my-auto px-5 py-3 rounded-2xl border-solid border-green-800">
+                      <img
+                        loading="lazy"
+                        src="https://cdn.builder.io/api/v1/image/assets/TEMP/66a13295-29a9-4ee5-8b30-7efda6cba5ce?apiKey=307f7ea7cf424aa8ac878e781b03700d&"
+                        className="aspect-square object-contain object-center w-5 overflow-hidden"
+                      />
+                    </div>
+                  </div>
+                  <div className="text-neutral-400 text-center text-xl font-semibold leading-7 whitespace-nowrap items-center border shadow-sm bg-white grow mt-1 px-5 py-3 rounded-2xl border-solid border-neutral-400">
+                    Submit
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+    }
+    
+    
 export default conversation_ai;
